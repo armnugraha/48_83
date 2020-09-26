bc1c7046c96f8bf23928a300c6af16572e910f32
Aar <armannugraha85@gmail.com>
2020-09-26 21:13:26 +0700

    update product

--- routes/products.js
+++ routes/products.js
@@ -17,16 +17,6 @@ const paginate = (query, { page, pageSize }) => {
     return {...query,offset,limit};
 };
 
-function authenticateToken(req, res, next) {
-  // Gather the jwt access token from the request header
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401) // if there isn't any token
-    var check = jwt.verifyLong(token, process.env.SECRET_KEY)
-    if (check.status == "error") return res.sendStatus(403)
-    next()
-}
-
 router.get('/', Auth, async (req, res, next) => {
  	const count = await Product.count({})
     const totalPage = Math.ceil(count/pageLimit)
